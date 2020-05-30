8e0f720f409c382a3686667fafec1e3b57eb094d
DeveshChau <deveshachaudhari@gmail.com>
2020-05-30 21:32:26 +0530

    over description last 10 balls

--- src/app/over-description/over-description.component.ts
+++ src/app/over-description/over-description.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../shared/service/data.service';
 import { Commentary } from '../shared/model/bookCricket.model';
-import { takeLast } from 'rxjs/operators';
 
 @Component({
   selector: 'app-over-description',
@@ -17,10 +16,8 @@ export class OverDescriptionComponent implements OnInit {
   }
   ngOnInit(): void {
     this.dataService.commentaryObs
-      // .pipe(takeLast(1))
       .subscribe(res => {
-        this.overDescription = res.length > 10 ? res.slice(-10) : res;
-        console.log(this.overDescription);
+        this.overDescription = res.slice(-10);
       });
   }
 
